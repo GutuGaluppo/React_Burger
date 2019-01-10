7b7fe91d11f796aae0cbf9b7d4d5e6a6ebb9226e
Gutu Galuppo <galuppodev@gmail.com>
2019-01-10 19:43:42 -0300

    NavigationItems in process...

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const navigationItem = (props) => (
+    <a href="/">A Link</a>
+);
+
+export default navigationItem;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navigationItems = () => (
+    <ul>
+        <li>
+            
+        </li>
+    </ul>
+);
+
+export default navigationItems;
